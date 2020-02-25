4ce886a8522831e66cc0d891aadcfe42fadf7e3a
Oluwafemi Alofe <oluwafemialofe@gmail.com>
2020-02-25 06:14:09 +0100

    Completed Template, Building AUthentication

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layout.partials._header')
+<body>
+<div class="d-flex" id="wrapper">
+    @section('sidenav')
+        @include('layout.partials._sidenav')
+    @show
+    <div id="page-content-wrapper">
+        @section('navbar')
+            @include('layout.partials._navbar')
+        @show
+
+        @yield('content')
+    </div>
+</div>
+@include('layout.partials._footer')
+</body>
+
+</html>
